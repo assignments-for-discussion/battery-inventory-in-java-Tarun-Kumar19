03551ae9ba254a267597eb1122c7516a7e8dc5df
Tarun Kumar <95239768+Tarun-Kumar19@users.noreply.github.com>
2023-12-03 17:12:41 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -1,28 +1,35 @@
 package bunchbysoh;
-
 public class Main {
-  static class CountsBySoH {
-    public int healthy = 0;
-    public int exchange = 0;
-    public int failed = 0;
-  };
+    static class CountsBySoH {
+        public int healthy = 0;
+        public int exchange = 0;
+        public int failed = 0;
+    }
+       static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
+        CountsBySoH counts = new CountsBySoH();
 
-  static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
-    CountsBySoH counts = new CountsBySoH();
-    return counts;
-  }
-
-  static void testBucketingByHealth() {
-    System.out.println("Counting batteries by SoH...\n");
-    int[] presentCapacities = {113, 116, 80, 95, 92, 70};
-    CountsBySoH counts = countBatteriesByHealth(presentCapacities);
-    assert(counts.healthy == 2);
-    assert(counts.exchange == 3);
-    assert(counts.failed == 1);
-    System.out.println("Done counting :)\n");
-  }
-
-  public static void main(String[] args) {
-    testBucketingByHealth();
-  }
+        for (int capacity: presentCapacities) {
+            double soh = 100.0 * capacity / 120.0;
+            if ( soh > 80.0 ) {
+                counts.healthy++;
+            } else if (soh >= 62.0 && soh <=80.0) {
+                counts.exchange++;
+            } else {
+                counts.failed++;
+            }
+        }
+        return counts;
+    }
+     static void testBucketingByHealth() {
+        System.out.println("Counting batteries by SoH...\n");
+        int[] presentCapacities = {113, 116, 80, 95, 92, 70};
+        CountsBySoH counts = countBatteriesByHealth(presentCapacities);
+        assert(counts.healthy == 2);
+        assert(counts.exchange == 3);
+        assert(counts.failed == 1);
+        System.out.println("Done counting :)\n");
+    }
+     public static void main(String[] args) {
+        testBucketingByHealth();
+    }
 }
